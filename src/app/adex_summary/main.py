74dc1d1be232347836957e95c8c2d923329d48a1
unioah <unioah@gmail.com>
2015-09-21 20:29:50 -0500

    add average

--- src/app/adex_summary/main.py
+++ src/app/adex_summary/main.py
@@ -1,4 +1,4 @@
-# adex summary generator
+# ADEX summary generator
 # author: unioah@gmail.com
 
 import ConfigParser as mParser
@@ -15,9 +15,9 @@ class ADEX_processor:
         self.child_age = ""
         self.child_gender = ""
         self.ADEX_head = []
-        self.items = ['AWC', 'Turn_Count', 'Child_Voc_Duration', 'CHN',
-                      'FAN', 'MAN', 'CXN', 'OLN', 'TVN', 'NON', 'SIL',
-                      'Audio_Duration']
+        self.items = {'AWC': 0, 'Turn_Count': 0, 'Child_Voc_Duration': 0,
+                      'CHN': 0, 'FAN': 0, 'MAN': 0, 'CXN': 0, 'OLN': 0,
+                      'TVN': 0, 'NON': 0, 'SIL': 0, 'Audio_Duration': 0}
 
     def get_key(self, row, key):
         key_index = self.ADEX_head.index(key)
@@ -51,32 +51,57 @@ class ADEX_processor:
             else:
                 end_lines -= 1
 
-        # add extra line in order add up to 1800 sec
+        # add extra line in order to add up to 1800 sec
         end_lines -= 1
         del self.content[end_lines:]
 
-    def filter(self):
+    def cal_average(self):
+        for row in self.content:
+            for col in range(0, len(row)):
+                key = self.ADEX_head[col]
+                if key in self.items:
+                    self.items[key] += float(row[col])
+
+        for i in self.items:
+            tmp = self.items[i] / len(self.content)
+            # keep only two digits
+            self.items[i] = float("{0:.2f}".format(tmp))
+            deg(i, self.items[i])
+
+    def filter_cols(self):
         self.filename = self.content[0][1]
         self.child_ID = self.content[0][4]
         self.child_age = self.content[0][5]
         self.child_gender = self.content[0][6]
+        new_content = []
 
-        # remove any columns not in self.items list
+        new_content.append([self.filename,
+                           self.child_ID,
+                           self.child_age,
+                           self.child_gender])
+
+        # write new head info
+        new_head = []
+        for col in range(7, len(self.ADEX_head)):
+            if self.ADEX_head[col] in self.items:
+                new_head.append(self.ADEX_head[col])
+
+        # take any columns in self.items list
         for row in self.content:
-            k = 0
+            new_row = []
             for col in range(7, len(row)):
-                deg(len(row))
-                if self.ADEX_head[col] != self.items[k]:
-                    del row[col]
-                else:
-                    k += 1
+                if self.ADEX_head[col] in self.items:
+                    new_row.append(row[col])
 
-            # remove first 6 columns
-            del row[:7]
+            new_content.append(new_row)
+
+        self.content = new_content
+        self.ADEX_head = new_head
 
     def run(self):
         self.remove_5mins()
-        self.filter()
+        self.cal_average()
+        self.filter_cols()
 
 
 class driver:
@@ -88,8 +113,9 @@ class driver:
             content = mParser.csv_reader(name)
             self.mADEX.ADEX_head = content[0]
             self.mADEX.content = content[1:]
+
         self.mADEX.run()
-        deg(self.mADEX.content)
+        mParser.csv_writer('/tmp/tmp.csv', self.mADEX.content)
 
 
 if __name__ == '__main__':
