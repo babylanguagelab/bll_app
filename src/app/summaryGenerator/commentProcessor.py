385525a174f5394914663f275603182aa7538433
unioah <unioah@gmail.com>
2016-06-16 21:26:41 -0500

    update comment processor

--- src/app/summaryGenerator/commentProcessor.py
+++ src/app/summaryGenerator/commentProcessor.py
@@ -1,111 +1,116 @@
 # Description:
-# Author: zhangh15@myumanitoba.ca
 # Date: 2016-01-17
 
 import logging as lg
 import openpyxl
 import ConfigParser as mParser
 
-COMMENT_OPTIONS = (("Recording Type" ,              ("Daycare", "Home", "Home Daycare")),
-                   ("ITS File" ,                    ()),
-                   ("Recording Notes/Errors?" ,     ("No", "Yes")),
-                   ("Note in File" ,                ("None")),
-                   ("Paired Recording" ,            ("No")),
-                   ("Language Other than English" , ("English", "French")),
-                   ("Child Sick" ,                  ("No", "Yes")),
-                   ("Child Development Concern" ,   ("No", "Yes")),
-                   ("Withdrew from Study" ,         ("No", "Yes")))
-
-COMMENT_OPTIONS_DICT = dict(COMMENT_OPTIONS)
-
 
 class CommentProcessor(object):
     def __init__(self, database):
-        self.content = {}
-        self.heads = []
-
+        self.content = {} # excel content info, include: head, body, column
         self.config = {'DB': database, 'filename': ""}
-
-        # 0 for no, 1 for default, 2 for second, -1 for all
-        self.switches = dict([[x[0], [True, 1]] for x in COMMENT_OPTIONS])
+        self.switch = {} # output info
+        self.output = []
 
     def open_comment_file(self):
         lg.debug("open comment file: " + self.config['filename'])
         workbook = openpyxl.load_workbook(self.config['filename'])
         sheet = workbook.get_sheet_by_name("Special Cases")
-        self.heads = [x.value for x in sheet.rows[0] if x.value is not None]
+        excel_head = [x.value for x in sheet.rows[0] if x.value is not None]
 
-        # if self.config['DB'].select('sqlite_master', ['name'],
-        #                             where="type='table' AND name='comment_summary'") is None:
-
-        #     param = self.output[0]
-        #     #param.insert(0, "ID INTEGER PRIMARY KEY AUTOINCREMENT")
-        #     param = ",".join(param)
-        #     sql = "CREATE TABLE ADEX_summary" + "(" + param + ",PRIMARY KEY (ChildID)" + ")"
-        #     self.config['DB'].execute_script(sql)
-
-        child_id = ""
+        # get content list
+        excel_body = []
         for rnum in range(2, sheet.max_row):
+            # sometimes the max_row number is not accurate, so I add an addition check
+            if sheet.cell(row=rnum, column=2).value is None:
+                break
+
             if sheet.cell(row=rnum, column=1).value is not None:
-                child_id = sheet.cell(row=rnum, column=1).value
-                self.content[child_id] = {}
+                child_dict = {}
+                for i, item in enumerate(excel_head):
+                    if sheet.cell(row=rnum, column=i+1).value is None:
+                        child_dict[item] = []
+                    else:
+                        child_dict[item] = [sheet.cell(row=rnum, column=i+1).value]
+                excel_body.append(child_dict)
+            else:
+                for i, item in enumerate(excel_head):
+                    if sheet.cell(row=rnum, column=i+1).value is not None:
+                        excel_body[-1][item].append(sheet.cell(row=rnum,
+                                                               column=i+1).value)
 
-            content_dict = {}
-            for i, item in enumerate(self.heads):
-                content_dict[item] = sheet.cell(row=rnum, column=i+1).value
+        # get option dictionary
+        excel_column = {}
+        for item in excel_body:
+            for name in excel_head:
+                if not excel_column.has_key(name):
+                    excel_column[name] = set(item[name])
+                else:
+                    excel_column[name] = excel_column[name].union(set(item[name]))
 
-            ifilename = sheet.cell(row=rnum, column=3).value
-            if ifilename is not None:
-                self.content[child_id][ifilename] = content_dict
+        self.content["head"] = excel_head
+        self.content["body"] = excel_body
+        self.content["column"] = excel_column
 
-    def get_options(self):
-        option_dict = {}
+    # switch for output options
+    def init_switch(self):
+        switch = {}
+        for item in self.content["head"]:
+            switch[item] = [True, self.content["column"][item]]
 
-        for item in self.heads:
-            option_dict[item] = self.get_options_from_title(item)
+        self.switch = switch
 
-        return option_dict
+    # update output options
+    def update_switch(self, item, enable,
+                      content="all", reverse=False):
 
-    def get_options_from_title(self, title):
-        CMT_options = []
+        original = self.content["column"][item]
 
-        if title == "Recording Notes/Errors?" or \
-           title == "Child Sick" or \
-           title == "Child Development Concern" or \
-           title == "Withdrew from Study":
-            CMT_options = ["Yes", "No"]
+        if enable is False:
+            self.switch[item] = [False, original]
+            return
 
-        elif title == "Language Other than English":
-            CMT_options.append("English")
-            for key1 in sorted(self.content.keys()):
-                for key2 in sorted(self.content[key1].keys()):
-                    note = self.content[key1][key2][title]
-                    if (note is not None) and (note not in CMT_options):
-                        CMT_options.append(note)
-            CMT_options.append("All")
+        if content == "all":
+            self.switch[item] = [True, original]
 
+        if reverse:
+            self.switch[item] = [True, set(original) - set(content)]
         else:
-            for key1 in sorted(self.content.keys()):
-                for key2 in sorted(self.content[key1].keys()):
-                    note = self.content[key1][key2][title]
-                    if note is not None and note not in CMT_options:
-                        CMT_options.append(note)
-
-        return CMT_options
-
-    #def filter_options_from_title(self, title, options)
-
+            self.switch[item] = [True, set(content)]
 
     # process switches and filter content
-    #def run(self):
+    def filter_switch(self):
+        remove_its = []
+
+        for item in self.content["head"]:
+            nfilter = self.content["column"] - self.switch[item]
+            for child in self.content["body"]:
+                for info in child[head]:
+                    if set(info).issubset(nfilter):
+                        remove_its.append(child["ITS File"])
+
+        new_output = []
+        for child in self.content["body"]:
+            for ITS in child["ITS File"]:
+                if ITS not in remove_its:
+                    for item in self.content["head"]:
+                        if self.switch[item][0] is True:
+                            new_output.append(child[item])
+
+                    self.output.append(new_output)
 
     def save_results(self, filename):
-        mParser.excel_writer(filename, 'Special Cases', self.content)
+        mParser.excel_writer(filename,
+                             'Special Cases',
+                             self.content)
 
 
 # for test
-#mCP = CommentProcessor("test")
-#mCP.config['filename'] = "/tmp/test/test.xlsx"
-#mCP.open_comment_file()
+mCP = CommentProcessor("test")
+mCP.config['filename'] = "/tmp/test.xlsx"
+mCP.open_comment_file()
+mCP.init_switch()
+print(mCP.content['head'])
 #options = mCP.get_options()
 #print(options["Child Sick"])
